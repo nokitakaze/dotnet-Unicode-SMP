409cbdfc8ec2d28aed9526fca50167df4f05e0e3
Nokita Kaze <admin@kanaria.ru>
2021-03-10 11:17:36 +0300

    * Change box' UnicodeRange to Extended version

--- RangeGenerator/Program.cs
+++ RangeGenerator/Program.cs
@@ -39,10 +39,10 @@ namespace RangeGenerator
                     var lastIndex = int.Parse(m.Groups[3].Value, NumberStyles.HexNumber);
 
                     return string.Format(
-                        "        public static UnicodeRange {0} =>\n" +
+                        "        public static UnicodeRangeExtended {0} =>\n" +
                         "            {0}_Ref ?? CreateRange(ref {0}_Ref, 0x{1:x6}, 0x{2:x6});\n" +
                         "\n" +
-                        "        private static UnicodeRange? {0}_Ref;",
+                        "        private static UnicodeRangeExtended? {0}_Ref;",
                         name,
                         firstIndex,
                         lastIndex
@@ -52,7 +52,6 @@ namespace RangeGenerator
                 .Aggregate((a, b) => a + "\n\n" + b);
 
             var fullText =
-                "using System.Text.Unicode;\n\n" +
                 "namespace UnicodeSMP\n" +
                 "{\n" +
                 "   public static partial class UnicodeRanges\n" +
