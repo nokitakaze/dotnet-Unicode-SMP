1d383d106ab1ae306fc4e367f5b9659f4019d97b
Nokita Kaze <admin@kanaria.ru>
2021-03-10 11:07:49 +0300

    * Change .Net Core

--- UnicodeSMP/UnicodeRanges.Partial.cs
+++ UnicodeSMP/UnicodeRanges.Partial.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Text.Unicode;
 using System.Threading;
@@ -8,13 +7,13 @@ namespace UnicodeSMP
     public static partial class UnicodeRanges
     {
         [MethodImpl(MethodImplOptions.NoInlining)] // the caller should be inlined, not this method
-        private static UnicodeRange CreateRange([NotNull] ref UnicodeRange? range, int first, int last)
+        private static UnicodeRange CreateRange(ref UnicodeRange? range, int first, int last)
         {
             return CreateRange(ref range, (char) first, (char) last);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)] // the caller should be inlined, not this method
-        private static UnicodeRange CreateRange([NotNull] ref UnicodeRange? range, char first, char last)
+        private static UnicodeRange CreateRange(ref UnicodeRange? range, char first, char last)
         {
             // It's ok if two threads race and one overwrites the other's 'range' value.
             Volatile.Write(ref range, UnicodeRange.Create(first, last));
